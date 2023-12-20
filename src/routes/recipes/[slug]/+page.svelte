96b061ceaa93bc290eb1fca83323f63f91d70c8b
timothykok <67496153+timothykok@users.noreply.github.com>
2023-12-20 15:46:49 +0800

    added recipe page buttons

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -13,6 +13,25 @@
     <div class="flex flex-row gap-8">
         <div>
             <img class="items-start pb-4 max-w-sm rounded" src={data.recipe.image} alt={data.recipe.title}>  
+            <div class="flex flex-row">
+                <div class="meal prep button">
+                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                        Button
+                      </button>
+                </div>
+                <div class = "save recipe button">
+                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                        Button
+                      </button>
+                </div>
+                <div class = "URL button">
+                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                        Button
+                      </button>
+                </div>
+    
+            </div>
+
 
             <p class="text-xl"> Summary </p>
             <div class='pr-14 pb-4 pt-3'>
