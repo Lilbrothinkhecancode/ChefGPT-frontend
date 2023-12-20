cc777cbcccd15b2625a250bcf40836ed7971223c
eisya <aisyahsulaiman98@gmail.com>
2023-12-20 16:31:20 +0800

    button layouts with cute icons

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -16,20 +16,31 @@
             <div class="flex flex-row">
                 <div class="meal prep button">
                     <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                        Button
+                        <svg viewBox="0 0 24 24" height="18" width="18">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+                        </svg>
+                            <span>Add to Meal Prep</span>
                       </button>
                 </div>
                 <div class = "save recipe button">
                     <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                        Button
+                        <svg viewBox="0 0 24 24" height="18" width="18">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"></path>
+                        </svg>
+                        <span>Save</span>
                       </button>
                 </div>
                 <div class = "URL button">
-                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                        Button
+                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">  
+                        <svg viewBox="0 0 24 24" height="18" width="18">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M18 16c-.79 0-1.5.31-2.03.81L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.53.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.48.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.05 4.12c-.05.22-.09.45-.09.69 0 1.66 1.34 3 3 3s3-1.34 3-3-1.34-3-3-3zm0-12c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"></path>
+                        </svg>
+                        <span>Share</span>
                       </button>
                 </div>
-    
             </div>
 
 
