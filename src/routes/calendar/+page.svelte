6bc64bec71adf40b9748606f522117b5922ad2b5
eisya <aisyahsulaiman98@gmail.com>
2023-12-22 14:48:11 +0800

    added you are not logged in on calender page

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -3,6 +3,7 @@
 	import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
 	import { onMount } from 'svelte';
 	import { IsLoggedIn } from '../../utils/stores.js';
+    import { goto } from '$app/navigation';
 
 	let userId = getUserId;
 	let mealPlans=[];
@@ -127,6 +128,12 @@
 	$: for (let i = 0; i < dateArray.length; i++) {
 		showDate[i] = dateArray[i].toLocaleDateString('en-GB');
 	}
+
+	function navigateToLogin() {
+    goto('users/login');
+    }
+	// "let's login first" navigation
+	
 </script>
 
 
@@ -169,8 +176,13 @@
 	</div>
 </div>
 {:else}
- <h1> YOU ARE NOT LOGGED IN </h1>
-	
+ <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+	<strong class="font-bold">Holy smokes!</strong>
+	<span class="block sm:inline">Let's <a on:click|preventDefault={navigateToLogin} class="underline text-red-500">login</a> first!</span>
+	<span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+	  <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+	</span>
+  </div>
 {/if}
 
 <style>
